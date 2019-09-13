01e6e3c8b16cb41b191257af5792c519412977ce
Gabriela Delfino <gabriela.delfino@outlook.com.br>
2019-09-13 19:56:47 -0300

    adiciona estrutura do zero para criar uma aplicacao em react

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import background from './assets/background.svg';
+
+class App extends Component {
+    render() {
+        return (
+            <img src={background} alt="ImgFundo" />
+        )
+    }
+}
+
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        path: path.join(__dirname, '/dist'),
+        filename: 'index_bundle.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                use: 'babel-loader',
+                exclude: /node_modules/
+            },
+            {
+                test: /\.svg$/,
+                use: ['@svgr/webpack','url-loader'],
+                exclude: /node_modules/
+            },
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './public/index.html'
+        })
+    ]
+}
